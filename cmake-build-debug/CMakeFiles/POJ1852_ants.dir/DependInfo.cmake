9887fea92f0da4ec27eb31db3e84a790002f8d90
TangHaomiao <1020145095@qq.com>
2022-01-28 18:51:28 +0800

    1.0

---
+++ cmake-build-debug/CMakeFiles/POJ1852_ants.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/allentang/Desktop/进行中/2022-spring-algorithm/POJ1852-ants/main.c" "/Users/allentang/Desktop/进行中/2022-spring-algorithm/POJ1852-ants/cmake-build-debug/CMakeFiles/POJ1852_ants.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/POJ1852_ants.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POJ1852_ants.dir/main.c.o"
+  "POJ1852_ants"
+  "POJ1852_ants.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/POJ1852_ants.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
